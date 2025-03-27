8797cb37260a783df208b669810f966f68830a97
eetgeenappels <eetgeenappels@gmail.com>
2025-03-27 23:20:16 +0100

    make leaves break when not close to WOOD

--- src/main/java/net/justmili/trueend/block/TreeLeavesBlock.java
+++ src/main/java/net/justmili/trueend/block/TreeLeavesBlock.java
@@ -1,12 +1,12 @@
-
 package net.justmili.trueend.block;
 
-import net.minecraft.world.phys.shapes.VoxelShape;
-import net.minecraft.world.phys.shapes.Shapes;
-import net.minecraft.world.phys.shapes.CollisionContext;
+import net.justmili.trueend.TrueEndMod;
+import net.justmili.trueend.init.TrueEndModBlocks;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.BlockState;
@@ -15,44 +15,31 @@ import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.SimpleWaterloggedBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.core.Direction;
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import java.util.Random;
 
 public class TreeLeavesBlock extends Block implements SimpleWaterloggedBlock {
 	public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
+	public static final IntegerProperty DISTANCE = IntegerProperty.create("distance", 1, 7);
 
 	public TreeLeavesBlock() {
-		super(BlockBehaviour.Properties.of().sound(SoundType.GRASS).strength(0.2f).noOcclusion().isRedstoneConductor((bs, br, bp) -> false));
-		this.registerDefaultState(this.stateDefinition.any().setValue(WATERLOGGED, false));
-	}
-
-	@Override
-	public boolean propagatesSkylightDown(BlockState state, BlockGetter reader, BlockPos pos) {
-		return state.getFluidState().isEmpty();
-	}
-
-	@Override
-	public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
-		return 0;
-	}
-
-	@Override
-	public VoxelShape getVisualShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
-		return Shapes.empty();
+		super(BlockBehaviour.Properties.of().sound(SoundType.GRASS).strength(0.2f).noOcclusion().isRedstoneConductor((bs, br, bp) -> false).randomTicks());
+		this.registerDefaultState(this.stateDefinition.any().setValue(WATERLOGGED, false).setValue(DISTANCE, 7));
 	}
 
 	@Override
 	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
 		super.createBlockStateDefinition(builder);
-		builder.add(WATERLOGGED);
+		builder.add(WATERLOGGED, DISTANCE);
 	}
 
 	@Override
 	public BlockState getStateForPlacement(BlockPlaceContext context) {
 		boolean flag = context.getLevel().getFluidState(context.getClickedPos()).getType() == Fluids.WATER;
-		return super.getStateForPlacement(context).setValue(WATERLOGGED, flag);
+		return super.getStateForPlacement(context).setValue(WATERLOGGED, flag).setValue(DISTANCE, 7);
 	}
 
 	@Override
@@ -61,15 +48,27 @@ public class TreeLeavesBlock extends Block implements SimpleWaterloggedBlock {
 	}
 
 	@Override
-	public BlockState updateShape(BlockState state, Direction facing, BlockState facingState, LevelAccessor world, BlockPos currentPos, BlockPos facingPos) {
-		if (state.getValue(WATERLOGGED)) {
-			world.scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickDelay(world));
+	public void tick(BlockState state, ServerLevel level, BlockPos blockPos, RandomSource randomSource) {
+		if (state.getValue(DISTANCE) == 7) {
+			if (randomSource.nextDouble() < 0.2) {
+				level.destroyBlock(blockPos, true);
+			}
 		}
-		return super.updateShape(state, facing, facingState, world, currentPos, facingPos);
 	}
 
 	@Override
-	public int getFlammability(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
-		return 30;
+	public BlockState updateShape(BlockState state, Direction facing, BlockState facingState, LevelAccessor world, BlockPos currentPos, BlockPos facingPos) {
+		int minDistance = 7;
+		for (Direction direction : Direction.values()) {
+			BlockState neighbor = world.getBlockState(currentPos.relative(direction));
+			if (neighbor.is(TrueEndModBlocks.WOOD.get())) {
+				minDistance = 1;
+				break;
+			} else if (neighbor.getBlock() instanceof TreeLeavesBlock) {
+				minDistance = Math.min(minDistance, neighbor.getValue(DISTANCE) + 1);
+			}
+		}
+		world.scheduleTick(currentPos, this, 20);
+		return state.setValue(DISTANCE, minDistance);
 	}
 }
