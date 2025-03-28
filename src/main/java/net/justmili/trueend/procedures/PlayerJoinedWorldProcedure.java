6bef3893f150cc55972ad41a1e1947321832deab
JustMili <rodx160@gmail.com>
2025-03-28 22:50:15 +0100

    modified some of PlayerJoinedWorld[rocedure.java

--- src/main/java/net/justmili/trueend/procedures/PlayerJoinedWorldProcedure.java
+++ src/main/java/net/justmili/trueend/procedures/PlayerJoinedWorldProcedure.java
@@ -1,37 +1,43 @@
 package net.justmili.trueend.procedures;
 
+import net.minecraftforge.server.ServerLifecycleHooks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.CommandSource;
+import net.minecraft.client.Minecraft;
 
 import javax.annotation.Nullable;
-
-// just a lil fun thing, will be useful for True End 1.4 ;)
 
 @Mod.EventBusSubscriber
 public class PlayerJoinedWorldProcedure {
 	@SubscribeEvent
 	public static void onPlayerLoggedIn(PlayerEvent.PlayerLoggedInEvent event) {
-		execute(event, event.getEntity());
+		execute(event, event.getEntity().level(), event.getEntity());
 	}
 
-	public static void execute(Entity entity) {
-		execute(null, entity);
+	public static void execute(LevelAccessor world, Entity entity) {
+		execute(null, world, entity);
 	}
 
-	private static void execute(@Nullable Event event, Entity entity) {
+	private static void execute(@Nullable Event event, LevelAccessor world, Entity entity) {
 		if (entity == null)
 			return;
-		if (!entity.level().isClientSide() && entity.getServer() != null) {
-			entity.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, entity.position(), entity.getRotationVector(), entity.level() instanceof ServerLevel ? (ServerLevel) entity.level() : null, 4,
-					entity.getName().getString(), entity.getDisplayName(), entity.level().getServer(), entity), "tellraw @a [\"\",{\"selector\":\"@s\",\"color\":\"yellow\"},{\"text\":\" joined the game\",\"color\":\"yellow\"}]");
+		String playerName = "";
+		if ((world.isClientSide() ? Minecraft.getInstance().getConnection().getOnlinePlayers().size() : ServerLifecycleHooks.getCurrentServer().getPlayerCount()) == 1) {
+			{
+				Entity _ent = entity;
+				if (!_ent.level().isClientSide() && _ent.getServer() != null) {
+					_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level() instanceof ServerLevel ? (ServerLevel) _ent.level() : null, 4,
+							_ent.getName().getString(), _ent.getDisplayName(), _ent.level().getServer(), _ent), "tellraw @a [\"\",{\"selector\":\"@s\",\"color\":\"yellow\"},{\"text\":\" joined the game\",\"color\":\"yellow\"}]");
+				}
+			}
 		}
-
 	}
 }
