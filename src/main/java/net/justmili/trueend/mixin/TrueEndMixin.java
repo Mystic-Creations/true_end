1db9df95891a9ecd9b8b6d1a9dd4281302a862b7
JustMili <rodx160@gmail.com>
2025-05-07 03:20:28 +0200

    some fix but still crashes ig

--- src/main/java/net/justmili/trueend/mixin/TrueEndMixin.java
+++ src/main/java/net/justmili/trueend/mixin/TrueEndMixin.java
@@ -5,9 +5,13 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(TARGETCLASS.class)
-public class TrueEndMixin {
-	@Inject(at = @At("HEAD"), method = "")
-	private void init(CallbackInfo info) {
-	}
+import net.minecraft.server.MinecraftServer;
+
+@Mixin(MinecraftServer.class)
+public abstract class TrueEndMixin {
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void onServerInit(CallbackInfo ci) {
+        // no-op
+    }
 }
