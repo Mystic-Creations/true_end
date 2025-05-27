90f7c81d0d35682c11bc6d5cd66df1f086cd7fa4
mahanhamisi <mahan.hamisi@gmail.com>
2025-05-28 00:04:46 +0330

    Fixed fog crashing issue

--- src/main/java/net/justmili/trueend/world/dimension/BeyondTheDream.java
+++ src/main/java/net/justmili/trueend/world/dimension/BeyondTheDream.java
@@ -1,7 +1,7 @@
-
 package net.justmili.trueend.world.dimension;
 
 import net.justmili.trueend.network.TrueEndVariables;
+import static net.justmili.trueend.network.TrueEndVariables.fogToggle;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.client.event.RegisterDimensionSpecialEffectsEvent;
@@ -12,27 +12,28 @@ import net.minecraft.world.phys.Vec3;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.client.renderer.DimensionSpecialEffects;
 
-@Mod.EventBusSubscriber
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class BeyondTheDream {
-	@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
-	public static class BeyondTheDreamSpecialEffectsHandler {
-		@SubscribeEvent
-		@OnlyIn(Dist.CLIENT)
-		public static void registerDimensionSpecialEffects(RegisterDimensionSpecialEffectsEvent event) {
-			//boolean fogToggle = TrueEndVariables.fogToggle.getValue();
-			DimensionSpecialEffects customEffect = new DimensionSpecialEffects(112f, true, DimensionSpecialEffects.SkyType.NONE, false, false) {
-				@Override
-				public Vec3 getBrightnessDependentFogColor(Vec3 color, float sunHeight) {
-					return color;
-				}
 
-				@Override
-				public boolean isFoggyAt(int x, int y) {
-					//return fogToggle;
-					return false;
-				}
-			};
-			event.register(ResourceLocation.parse("true_end:beyond_the_dream"), customEffect);
-		}
-	}
+    @SubscribeEvent
+    @OnlyIn(Dist.CLIENT)
+    public static void registerDimensionSpecialEffects(RegisterDimensionSpecialEffectsEvent event) {
+        DimensionSpecialEffects customEffect = new DimensionSpecialEffects(112f, true, DimensionSpecialEffects.SkyType.NONE, false, false) {
+            
+            @Override
+            public Vec3 getBrightnessDependentFogColor(Vec3 color, float sunHeight) {
+                return color;
+            }
+
+            public boolean isFoggyAt(int x, int y, int z) {
+                return (fogToggle != null && fogToggle.getValue());
+            }
+
+            @Override
+            public boolean isFoggyAt(int p_108874_, int p_108875_) {
+                throw new UnsupportedOperationException("Not supported yet.");
+            }
+        };
+        event.register(new ResourceLocation("true_end", "beyond_the_dream"), customEffect);
+    }
 }
