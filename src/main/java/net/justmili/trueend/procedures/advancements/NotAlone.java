55f02d4e4a0d96c97030b68fca3810d671d660fe
JustMili <rodx160@gmail.com>
2025-05-28 17:49:12 +0200

    New advancement

---
+++ src/main/java/net/justmili/trueend/procedures/advancements/NotAlone.java
@@ -0,0 +1,47 @@
+package net.justmili.trueend.procedures.advancements;
+
+import net.justmili.trueend.network.TrueEndVariables;
+import net.minecraft.advancements.Advancement;
+import net.minecraft.advancements.AdvancementProgress;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.Entity;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+import javax.annotation.Nullable;
+
+import static net.justmili.trueend.regs.DimKeyRegistry.BTD;
+
+public class NotAlone {
+    @SubscribeEvent
+    public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
+        if (event.phase == TickEvent.Phase.END) {
+            execute(event, event.player);
+        }
+    }
+
+    public static void execute(Entity entity) {
+        execute(null, entity);
+    }
+
+    private static void execute(@Nullable Event event, Entity entity) {
+        if (entity == null)
+            return;
+        if (TrueEndVariables.randomEventsToggle.getValue()) {
+            if (Math.random() < TrueEndVariables.randomEventChance.getValue() / 2) {
+                if ((entity.level().dimension()) == BTD) {
+                    if (entity instanceof ServerPlayer _player) {
+                        Advancement _adv = _player.server.getAdvancements().getAdvancement(ResourceLocation.parse("true_end:not_alone"));
+                        AdvancementProgress _ap = _player.getAdvancements().getOrStartProgress(_adv);
+                        if (!_ap.isDone()) {
+                            for (String criteria : _ap.getRemainingCriteria())
+                                _player.getAdvancements().award(_adv, criteria);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
