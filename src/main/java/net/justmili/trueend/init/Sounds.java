9dd16128f6654dc4115d661fac4c1b5fd54a1f5d
mahanhamisi <mahan.hamisi@gmail.com>
2025-07-08 19:21:10 +0330

    discs are working

--- src/main/java/net/justmili/trueend/init/Sounds.java
+++ src/main/java/net/justmili/trueend/init/Sounds.java
@@ -1,18 +1,20 @@
 package net.justmili.trueend.init;
 
-import net.minecraftforge.registries.RegistryObject;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.DeferredRegister;
-
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.resources.ResourceLocation;
-
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import net.justmili.trueend.TrueEnd;
 
 public class Sounds {
-    public static final DeferredRegister<SoundEvent> REGISTRY = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, TrueEnd.MODID);
-    public static final RegistryObject<SoundEvent> VINE_BOOM = REGISTRY.register("easter_egg.vine_boom", () -> SoundEvent.createVariableRangeEvent(ResourceLocation.parse("true_end:easter_egg.vine_boom")));
-    public static final RegistryObject<SoundEvent> MOD_CREDITS_MUSIC = REGISTRY.register("music.modcredits", () -> SoundEvent.createVariableRangeEvent(ResourceLocation.parse("true_end:mod_credits")));
-    public static final RegistryObject<SoundEvent> MUSIC_FARLANDS = REGISTRY.register("music_disc.farlands", () -> SoundEvent.createVariableRangeEvent(ResourceLocation.parse("true_end:mod_credits")));
-    public static final RegistryObject<SoundEvent> MUSIC_NEVER_ALONE = REGISTRY.register("music_disc.never_alone", () -> SoundEvent.createVariableRangeEvent(ResourceLocation.parse("true_end:mod_credits")));
+    public static final DeferredRegister<SoundEvent> REGISTRY =
+        DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, TrueEnd.MODID);
+    private static RegistryObject<SoundEvent> sound(String name) {
+        return REGISTRY.register(name, () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(TrueEnd.MODID, name)));
+    }
+    public static final RegistryObject<SoundEvent> VINE_BOOM         = sound("vine_boom");
+    public static final RegistryObject<SoundEvent> MOD_CREDITS_MUSIC = sound("back_in_the_game");
+    public static final RegistryObject<SoundEvent> MUSIC_FARLANDS    = sound("farlands");
+    public static final RegistryObject<SoundEvent> MUSIC_NEVER_ALONE = sound("never_alone");
 }
