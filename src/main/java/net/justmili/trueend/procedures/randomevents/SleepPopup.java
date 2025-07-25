c3fddd67ed7ea21957964e6b672f635afb1d4f67
mahanhamisi <mahan.hamisi@gmail.com>
2025-07-25 21:35:19 +0330

    clean up of popop

--- src/main/java/net/justmili/trueend/procedures/randomevents/SleepPopup.java
+++ src/main/java/net/justmili/trueend/procedures/randomevents/SleepPopup.java
@@ -1,51 +1,45 @@
 package net.justmili.trueend.procedures.randomevents;
 
-import net.justmili.trueend.interfaces.User32;
-import net.justmili.trueend.network.Variables;
-import net.justmili.trueend.TrueEnd;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.level.Level;
-import net.minecraftforge.event.entity.player.PlayerSleepInBedEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
+import java.awt.Dialog;
 
 import javax.swing.JDialog;
 import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
-import java.awt.*;
+
+import net.justmili.trueend.interfaces.User32;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraftforge.event.entity.player.PlayerSleepInBedEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber
 public class SleepPopup {
-	@SubscribeEvent
-	public static void onPlayerInBed(PlayerSleepInBedEvent event) {
-		ServerPlayer player = (ServerPlayer) event.getEntity();
-		if (!Variables.randomEventsToggle) return;
-		if (!Variables.popupsToggle) return;
-		if (!(Math.random() < Variables.randomEventChance)) return;
-		if (player.level().dimension() != Level.OVERWORLD) return;
 
-		if (player.isSleeping()) {
-			String osName = System.getProperty("os.name").toLowerCase();
-			if (osName.contains("win")) {
-				TrueEnd.wait(20, () -> {
-					User32.INSTANCE.MessageBoxA(0L, "wake up.", "", 0);
-				});
-			} else {
-				TrueEnd.wait(20, () -> {
-					SwingUtilities.invokeLater(() -> {
-						String message = "wake up.";
-						String title = "";
+@SubscribeEvent
+public static void onPlayerInBed(PlayerSleepInBedEvent event) {
+    if (!(event.getEntity() instanceof ServerPlayer player)) return;
 
-						JOptionPane pane = new JOptionPane(message, JOptionPane.INFORMATION_MESSAGE);
+    System.out.println("Sleep event fired!");
 
-						JDialog dialog = pane.createDialog(null, title);
-						dialog.setAlwaysOnTop(true);
-						dialog.setModal(true);
-						dialog.setModalExclusionType(Dialog.ModalExclusionType.NO_EXCLUDE);
-						dialog.setVisible(true);
-					});
-				});
-			}
-		}
-	}
+    new Thread(() -> {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        String osName = System.getProperty("os.name").toLowerCase();
+        if (osName.contains("win")) {
+            User32.INSTANCE.MessageBoxA(0L, "wake up.", "", 0);
+        } else {
+            SwingUtilities.invokeLater(() -> {
+                JOptionPane pane = new JOptionPane("wake up.", JOptionPane.INFORMATION_MESSAGE);
+                JDialog dialog = pane.createDialog(null, "");
+                dialog.setAlwaysOnTop(true);
+                dialog.setModal(true);
+                dialog.setModalExclusionType(Dialog.ModalExclusionType.NO_EXCLUDE);
+                dialog.setVisible(true);
+            });
+        }
+    }).start();
+  }
 }
