4669394910c2ff94c710c9ba68a836d4c6f03d49
eetgeenappels <eetgeenappels@gmail.com>
2025-07-27 02:16:46 +0200

    fix stuff

--- src/main/java/net/justmili/trueend/procedures/events/BlackScreenLiminalForest.java
+++ src/main/java/net/justmili/trueend/procedures/events/BlackScreenLiminalForest.java
@@ -6,6 +6,7 @@ import net.justmili.trueend.init.Biomes;
 import net.justmili.trueend.network.Variables;
 import net.justmili.trueend.procedures.devcmd.screentests.TestBlackOverlay;
 import net.minecraft.core.Direction;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
@@ -21,7 +22,7 @@ import org.spongepowered.asm.mixin.MixinEnvironment;
 
 import java.util.Random;
 
-@Mod.EventBusSubscriber(value = Dist.CLIENT)
+@Mod.EventBusSubscriber
 public class BlackScreenLiminalForest {
 
     private static Random random = new Random();
@@ -29,22 +30,17 @@ public class BlackScreenLiminalForest {
     @SubscribeEvent
     public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
 
-        if (event.side != LogicalSide.CLIENT) {
-            return;
-        }
-
         Player player = event.player;
 
-        if (player.level().getBiome(player.blockPosition()) == Biomes.SEEPING_REALITY) {
+        if (player.level().getBiome(player.blockPosition()).is(Biomes.SEEPING_REALITY)) {
 
             player.getCapability(Variables.PLAYER_VARS_CAP).ifPresent(cap -> {
                 int liminalTime = cap.getLiminalForestTime() + 1;
                 cap.setLiminalForestTime(liminalTime);
 
                 double chance =  random.nextDouble();
-
-                if (chance < Variables.randomEventChance * Math.log(liminalTime) / Math.log(600)) {
-
+                player.sendSystemMessage(Component.literal(Double.toString(Variables.randomEventChance * Math.log(liminalTime) / Math.log(600))));
+                if (chance < Variables.randomEventChance * Math.log(liminalTime) / Math.log(2400)) {
                     TestBlackOverlay.execute(player.level(), player.getX(), player.getY(), player.getZ(), player);
                 }
             });
